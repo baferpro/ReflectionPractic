154725249c25e6f1683462bb67b6b403d56b3993
Faust <baferpro@gmail.com>
2021-05-13 19:22:20 +0300

    Добавьте файлы проекта.

---
+++ ReflectionPractic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionPractic
+{
+    class Program
+    {
+        public static db g_db = new db();
+        static void Main(string[] args)
+        {
+            string select = g_db.Shopping.OrderByDescending(i => i.Summa).Select(i => i.Buyer.Name).FirstOrDefault().ToString();
+            Console.WriteLine(select);
+            Console.ReadLine();
+        }
+    }
+}
